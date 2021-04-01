d82526880ff759a0a84b7b3a5e42d74768c9352a
EzanAsif <ezanasif1972@gmail.com>
2021-04-01 23:09:38 +0500

    added 2nd section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import HeroSection from './Components/HeroSection/HeroSection'
 import SiteHeader from './Components/Header/SiteHeader'
-
+import Services from './Components/Services/Services'
 import './App.css'
 function App() {
   return (
     <header className="App">
       <SiteHeader/>
       <HeroSection/>
+      <Services/>
     </header>
   );
 }
